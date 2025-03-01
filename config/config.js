94f239c65c5ba3d636d07fa680b5223723c7f42e
dynamicrevs <153166663+dynamicrevs@users.noreply.github.com>
2025-03-01 20:23:02 +0530

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+  telegramBotToken: process.env.TELEGRAM_BOT_TOKEN,
+  telegramGroupId: process.env.TELEGRAM_GROUP_ID,
+  databasePath: process.env.DATABASE_PATH || './database/sqlite.db',
+  port: process.env.PORT || 3000,
+  workerTimeout: 3600000, // 1 hour in milliseconds
+  reportInterval: 604800000, // 7 days in milliseconds
+};
