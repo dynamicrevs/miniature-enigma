0471d24f86a0c1b07b9b51eca3da26935ace8fd0
dynamicrevs <153166663+dynamicrevs@users.noreply.github.com>
2025-03-01 20:35:36 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('./config/config');
+const workerRoutes = require('./routes/worker');
+const telegramRoutes = require('./routes/telegram');
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/worker', workerRoutes);
+app.use('/telegram', telegramRoutes);
+
+app.listen(config.port, () => console.log(`Controller running on port ${config.port}`));
